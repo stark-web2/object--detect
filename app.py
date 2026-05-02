16e3455cae4d2e21579f47fd7dc082e56fc4f546
Crystal Bangalisan <crystalbangalisan64@gmail.com>
2026-05-03 00:04:17 +0800

    Reformat app.py with consistent import statements

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class YOLOProcessor(VideoProcessorBase):
         annotated_frame = results[0].plot()
 
         return av.VideoFrame.from_ndarray(annotated_frame, format="bgr24")
-
+        
 webrtc_streamer(
     key="yolo-live",
     video_processor_factory=YOLOProcessor,
